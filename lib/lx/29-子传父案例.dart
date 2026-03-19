602de4bf151e3d229c4284b7127ab7a53c78ad1e
dongtao <dongtaocom@126.com>
2026-03-19 08:12:57 +0800

    提子传父案例

---
+++ lib/lx/29-子传父案例.dart
@@ -0,0 +1,87 @@
+
+import 'package:flutter/material.dart';
+void main(List<String>args){
+  runApp(MainPage());
+}
+
+//父组件
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  List<String> _list = [
+    "猪肉炖血肠",
+    "小鸡炖蘑菇",
+    "排骨炖豆角",
+    "扣肉",
+    "豆腐鱿鱼"];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: GridView.count(
+          padding: EdgeInsets.all(10),
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+          crossAxisCount: 2,
+          children: List.generate(_list.length, (int index){
+            return Child(foodName: _list[index],index: index, delFood: (int i) {
+              print("---父组件----");
+              print(i);
+              _list.removeAt(i);
+              setState(() {
+
+              });
+              print("---父组件----");
+            },);
+
+          }),
+        ),
+
+      ),
+    );
+  }
+}
+
+
+
+//子组件
+class Child extends StatefulWidget {
+  final String foodName;
+  final int index;
+  final Function (int index) delFood; //声明函数属性
+  const Child({super.key,required this.foodName,required this.index,required this.delFood});
+
+  @override
+  State<Child> createState() => _ChildState();
+}
+
+class _ChildState extends State<Child> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.topRight,
+      children: [
+        Container(
+          color: Colors.blue,
+          alignment: Alignment.center,
+          child:Text(widget.foodName,style: TextStyle(color: Colors.white,fontSize: 20)
+          ),
+        ),
+        IconButton(
+            color: Colors.red,
+            onPressed: (){
+              print("---按钮中----");
+              print(widget.index);
+              print(widget.foodName);
+              widget.delFood(widget.index);
+              print("---按钮结束----");
+            }, icon: Icon(Icons.delete)),
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,13 @@
 
-import 'package:flutter/material.dart';
+
+
+import 'package:dio/dio.dart';
+
 void main(List<String>args){
-  runApp(MainPage());
-}
+  Dio().get("https://geek.itheima.net/v1_0/channels").then((res){
+      print(res);
 
-//父组件
-class MainPage extends StatefulWidget {
-  const MainPage({super.key});
-
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  List<String> _list = [
-      "猪肉炖血肠",
-      "小鸡炖蘑菇",
-      "排骨炖豆角",
-       "扣肉",
-      "豆腐鱿鱼"];
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-          body: GridView.count(
-            padding: EdgeInsets.all(10),
-            mainAxisSpacing: 10,
-              crossAxisSpacing: 10,
-              crossAxisCount: 2,
-            children: List.generate(_list.length, (int index){
-              return Child(foodName: _list[index],);
-            }),
-          ),
-          
-        ),
-    );
-  }
-}
-
-
-
-//子组件
-class Child extends StatefulWidget {
-  final String foodName;
-  const Child({super.key,required this.foodName});
-
-  @override
-  State<Child> createState() => _ChildState();
-}
-
-class _ChildState extends State<Child> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
-        alignment: Alignment.center,
-        child:Text(widget.foodName,style: TextStyle(color: Colors.white,fontSize: 20)),
-    );
-  }
+  }).catchError((error){
+      print(error);
+  });
 }
